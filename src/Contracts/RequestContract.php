e006618033bfa910fa02682e607c438e4b508a42
Bob Horvers <bobhorvers@gmail.com>
2016-05-20 11:31:22 +0200

    Updated RequestContract to reflect changes

--- src/Contracts/RequestContract.php
+++ src/Contracts/RequestContract.php
@@ -7,147 +7,148 @@ interface RequestContract
     /**
      * Staticly create a new Request instance.
      *
-     * @param string $method
-     * @param string $url
-     * @param string $body
+     * @param  string $method
+     * @param  string $url
+     * @param  string $body
+     * @return Request
      */
     public static function make($method, $url, $body, $options);
 
     /**
      * Create a new Request instance for quick access.
      *
-     * @param string $method
-     * @param string $body
-     * @param array  $headers
-     * @param array  $cookies
-     * @param array  $options
+     * @param  string $method
+     * @param  string $body
+     * @param  array  $headers
+     * @param  array  $cookies
+     * @param  array  $options
+     * @return Request
      */
     public static function makeRequest($method, $url, $body, $headers, $cookies, $options);
 
     /**
-     * Send a GET request with minimal configuration.
+     * Compose a GET request.
      *
-     * @param string $url
-     * @param array  $headers
-     * @param array  $cookies
-     * @param array  $options
+     * @param  string $url
+     * @return Request
      */
-    public static function get($url, $headers, $cookies, $options);
+    public static function get($url);
 
     /**
-     * Send a POST request with minimal configuration.
+     * Compose a POST request.
      *
-     * @param string $url
-     * @param string $body
-     * @param array  $headers
-     * @param array  $cookies
-     * @param array  $options
+     * @param  string $url
+     * @param  string $body
+     * @return Request
      */
-    public static function post($url, $body, $headers, $cookies, $options);
+    public static function post($url, $body = '');
 
     /**
-     * Send a PUT request with minimal configuration.
+     * Compose a PUT request.
      *
-     * @param string $url
-     * @param string $body
-     * @param array  $headers
-     * @param array  $cookies
-     * @param array  $options
+     * @param  string $url
+     * @param  string $body
+     * @return Request
      */
-    public static function put($url, $body, $headers, $cookies, $options);
+    public static function put($url, $body = '');
 
     /**
-     * Send a PATCH request with minimal configuration.
+     * Compose a PATCH request.
      *
-     * @param string $url
-     * @param string $body
-     * @param array  $headers
-     * @param array  $cookies
-     * @param array  $options
+     * @param  string $url
+     * @param  string $body
+     * @return Request
      */
-    public static function patch($url, $body, $headers, $cookies, $options);
+    public static function patch($url, $body = '');
 
     /**
-     * Send a DELETE request with minimal configuration.
+     * Compose a DELETE request.
      *
-     * @param string $url
-     * @param string $body
-     * @param array  $headers
-     * @param array  $cookies
-     * @param array  $options
+     * @param  string $url
+     * @param  string $body
+     * @return Request
      */
-    public static function delete($url, $body, $headers, $cookies, $options);
+    public static function delete($url, $body = '');
 
     /**
      * Set the HTTP-authentication method to basic.
      *
-     * @param string $username
-     * @param string $password
+     * @param  string $username
+     * @param  string $password
+     * @return Request
      */
     public function setBasicAuthentication($username, $password);
 
     /**
      * Set the HTTP-authentication method to digest.
      *
-     * @param string $username
-     * @param string $password
+     * @param  string $username
+     * @param  string $password
+     * @return Request
      */
     public function setDigestAuthentication($username, $password);
 
     /**
      * Set the referer of the request.
      *
-     * @param string $referer
+     * @param  string $referer
+     * @return Request
      */
     public function setReferer($referer);
 
     /**
      * Set the user agent of the request.
      *
-     * @param string $agent
+     * @param  string $agent
+     * @return Request
      */
     public function setUserAgent($agent);
 
     /**
      * Set the request content mimetype.
      *
-     * @param string $type
+     * @param  string $type
+     * @return Request
      */
     public function sends($type);
 
     /**
      * Set the expected response format.
      *
-     * @param string $type
+     * @param  string $type
+     * @return Request
      */
     public function expects($type);
 
     /**
      * Set the body for the request;
      *
-     * @param string $body
+     * @param  string $body
+     * @return Request
      */
     public function setBody($body);
 
     /**
      * Set an additional curl option.
      *
-     * @param int    $option
-     * @param string $value
+     * @param  int    $option
+     * @param  string $value
+     * @return Request
      */
     public function setOption($option, $value);
 
     /**
      * Set additional curl options.
      *
-     * @param array $options
+     * @param  array $options
+     * @return Request
      */
     public function setOptions(array $options);
 
     /**
      * Execute the request.
      *
-     * @return mixed
+     * @return Response
      * @throws Exception
      */
     public function send();
